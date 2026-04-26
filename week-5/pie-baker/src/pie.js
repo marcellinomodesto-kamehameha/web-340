a11afdd1cf551e2e7bf43c87b0835eff1f20d5f6
Marcellino <marcellinomodesto@gmail.com>
2026-04-26 07:56:24 -0700

    feat: completed pie-baker assignment for week 5

--- week-5/pie-baker/src/pie.js
+++ week-5/pie-baker/src/pie.js
@@ -1,13 +1,22 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author: Marcellino Modesto
+ * Date: 4/25/2026
+ * File Name: pie.js
+ * Description: Creating the bakePie function
  */
 "use strict";
 
+
 function bakePie(pieType, ingredients) {
-  // Your code here
+  const essentials = ['flour', 'sugar', 'butter'];
+  const missing = essentials.filter(item => !ingredients.includes(item));
+
+  if (missing.length > 0) {
+    console.warn(`Missing essential ingredient: ${missing[0]}`);
+    process.exit(1);
+  }
+
+  return `Successfully baked a ${pieType} pie!`;
 }
 
-module.exports = { bakePie };
+module.exports = bakePie;
--- week-5/pie-baker/test/pie.spec.js
+++ week-5/pie-baker/test/pie.spec.js
@@ -1,12 +1,40 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author: Marcellino Modesto
+ * Date: 4/25/2026
+ * File Name: pie.spec.js
+ * Description: Creating unit tests
  */
 
 "use strict";
 
-const { bakePie } = require("../src/pie");
+const bakePie = require("../src/pie");
 
-// Your tests here
+describe('bakePie', () => {
+
+  test('should return success message when all ingredients are provided', () => {
+    const ingredients = ['flour', 'sugar', 'butter', 'apples'];
+    const result = bakePie('apple', ingredients);
+    expect(result).toBe('Successfully baked a apple pie!');
+  });
+
+  test('should bake pie even with extra ingredients', () => {
+    const ingredients = ['flour', 'sugar', 'butter', 'cherries', 'salt'];
+    const result = bakePie('cherry', ingredients);
+    expect(result).toBe('Successfully baked a cherry pie!');
+  });
+
+  test('should call process.exit(1) and log a warning if essentials are missing', () => {
+    const exitMock = jest.spyOn(process, 'exit').mockImplementation(() => {});
+    const logMock = jest.spyOn(console, 'warn').mockImplementation(() => {});
+
+    const ingredients = ['apples'];
+    bakePie('apple', ingredients);
+
+    expect(logMock).toHaveBeenCalledWith('Missing essential ingredient: flour');
+    expect(exitMock).toHaveBeenCalledWith(1);
+
+    exitMock.mockRestore();
+    logMock.mockRestore();
+  });
+
+});
