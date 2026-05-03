6cfe030925ebdca7f192538da1960e2a27684e74
Marcellino <marcellinomodesto@gmail.com>
2026-05-03 14:48:48 -0700

    feat: completed week 6 fantasy game character creation

--- week-6/fantasy-game-character-creation/src/server.js
+++ week-6/fantasy-game-character-creation/src/server.js
@@ -1,14 +1,57 @@
-const http = require('http');
-const url = require('url');
+"use strict";
 
-// TODO: Implement your server here
+const http = require("http");
+const url = require("url");
+
+let character = null;
 
 const server = http.createServer((req, res) => {
-  // TODO: Implement your routes here
+  const parsedUrl = url.parse(req.url, true);
+  const pathname = parsedUrl.pathname;
+
+  if (req.method === "POST" && pathname === "/create") {
+    const { class: charClass, gender, fact } = parsedUrl.query;
+
+    character = {
+      class: charClass,
+      gender,
+      fact
+    };
+
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Character created", character }));
+  }
+
+  else if (req.method === "POST" && pathname === "/confirm") {
+    if (!character) {
+      res.writeHead(400);
+      return res.end("No character to confirm");
+    }
+
+    res.writeHead(200);
+    res.end("Character confirmed!");
+  }
+
+  else if (req.method === "GET" && pathname === "/character") {
+    if (!character) {
+      res.writeHead(404);
+      return res.end("Character not found");
+    }
+
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(character));
+  }
+
+  else {
+    res.writeHead(404);
+    res.end("Route not found");
+  }
 });
 
-server.listen(3000, () => {
-  console.log('Server listening on port 3000');
-});
+if (require.main === module) {
+  server.listen(3000, () => {
+    console.log("Server running on port 3000");
+  });
+}
 
 module.exports = server;
--- week-6/fantasy-game-character-creation/test/server.spec.js
+++ week-6/fantasy-game-character-creation/test/server.spec.js
@@ -1,4 +1,64 @@
+"use strict";
+
 const http = require('http');
 const server = require('../src/server');
 
-// TODO: Implement your tests here
+beforeAll((done) => {
+  server.listen(3000, done);
+});
+
+afterAll((done) => {
+  server.close(done);
+});
+
+beforeEach((done) => {
+  const req = http.request({
+    hostname: "localhost",
+    port: 3000,
+    path: "/create?class=Warrior&gender=Male&fact=Strong",
+    method: "POST"
+  }, () => {
+    done();
+  });
+
+  req.end();
+});
+
+describe("Fantasy Character API", () => {
+
+  test("POST /create should create a character", (done) => {
+    const req = http.request({
+      hostname: "localhost",
+      port: 3000,
+      path: "/create?class=Warrior&gender=Male&fact=Strong",
+      method: "POST"
+    }, (res) => {
+      expect(res.statusCode).toBe(200);
+      done();
+    });
+
+    req.end();
+  });
+
+  test("POST /confirm should confirm a character", (done) => {
+    const req = http.request({
+      hostname: "localhost",
+      port: 3000,
+      path: "/confirm",
+      method: "POST"
+    }, (res) => {
+      expect(res.statusCode).toBe(200);
+      done();
+    });
+
+    req.end();
+  });
+
+  test("GET /character should return created character", (done) => {
+    http.get("http://localhost:3000/character", (res) => {
+      expect(res.statusCode).toBe(200);
+      done();
+    });
+  });
+
+});
